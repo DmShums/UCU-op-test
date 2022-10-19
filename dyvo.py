89ed390c460c3a3c566f83c69cd878ef4583f763
mikl123 <bulesnmisha3@gmil.com>
2022-10-19 13:06:25 +0300

    Added dyvo functions.

---
+++ dyvo.py
@@ -0,0 +1,15 @@
+def dyvo_insert(sentence, flag):
+    """
+    Inserting word "диво" before every word that starts with flag.
+    >>> dyvo_insert("Кит кота по хвилях катав - кит у воді, кіт на киті","ки")
+    Диво кит кота по хвилях катав - диво кит у воді, кіт на диво киті
+    """
+    buf=""
+    sentence=sentence.lower()
+    for i in sentence.split(" "):
+        if i[0:2]==flag:
+            buf+="диво "+i+" "
+        else:
+            buf+=i+" "
+    return buf.capitalize().strip()
+print(dyvo_insert("Кит кота по хвилях катав - кит у воді, кіт на киті","ки"))
